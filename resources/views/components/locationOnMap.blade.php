39e26814729cb1e57786d0f7e57fdd763cee830b
navid hero <navid_hero_1st@yahoo.com>
2026-07-02 17:10:53 +0330

    new office location on map

--- resources/views/components/locationOnMap.blade.php
+++ resources/views/components/locationOnMap.blade.php
@@ -1,1 +1,4 @@
-<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12189.517112698824!2d44.4989535!3d40.2006254!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd3b7680f83f045b!2s62%C2%B0%20Asteechan%20Architects!5e0!3m2!1sen!2s!4v1624781739622!5m2!1sen!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+<iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3046.534798125105!2d44.568577675906795!3d40.219409971471954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406aa3006ba0fe37%3A0x7bd62969535d41f9!2sBabajanyan%2038!5e0!3m2!1sen!2s!4v1782999552769!5m2!1sen!2s"
+  width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+  referrerpolicy="strict-origin-when-cross-origin"></iframe>
